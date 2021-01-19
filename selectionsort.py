fba658d86011b1e68414cf4a3a140c0dd6c35a14
KamruzzamanUzzal <kamruzzaman080@protonmail.com>
2021-01-19 20:10:20 +0600

    a simple selection sort code.

--- selectionsort.py
+++ selectionsort.py
@@ -1,5 +1,5 @@
 def sort(nums):
-    for i in range(len(nums)-1):
+    for i in range(len(nums) - 1):
         minpos = i
         k = len(nums)
         for j in range(i, k):
@@ -10,15 +10,24 @@ def sort(nums):
         nums[minpos] = temp
 
 
-lst = []
+def input_data():
+    i = input("\nEnter the numbers : ").strip().split()
+    return i
 
+# Basic way of getting input from the user
+# n = int(input("Enter number of elements : "))
+# iterating till the range
+# for i in range(0, n):
+#    ele = int(input())
+#    lst.append(ele)  # adding the element
+# print(lst)
 
-n = int(input("Enter number of elements : "))
-
-
-for i in range(0, n):
-    ele = int(input())
-
-    lst.append(ele)
-sort(lst)
-print(lst)
+# using map and exception handling
+try:
+    print("Please use a [space] between each input.")
+    lst = list(map(int, input_data()))
+    print("Unsorted list: ", lst)
+    sort(lst)
+    print("Sorted list: ", lst)
+except:
+    print("Wrong inputs. Only use Int values")

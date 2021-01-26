fdece9c1b1bac7512c2e4159a180ee53263a01c5
KamruzzamanUzzal <kamruzzaman080@protonmail.com>
2021-01-26 19:01:38 +0600

    finding a line of code inside it's own file(or diff file)

--- lookingfor_a_line.py
+++ lookingfor_a_line.py
@@ -1,3 +1,18 @@
+def search_multiple_strings_in_file(file_name, list_of_strings):
+    line_number = 0
+    list_of_results = []
+    with open(file_name, 'r') as read_obj:
+        for line in read_obj:
+            line_number += 1
+            for string_to_search in list_of_strings:
+                if string_to_search in line:
+                    list_of_results.append((string_to_search, line_number, line.rstrip()))
+                    return list_of_results
+                else:
+                    strout = "Not found."
+                    return strout
+
+
 def search_string_in_file(file_name, string_to_search):
     line_number = 0
     list_of_results = []
@@ -13,6 +28,7 @@ def search_string_in_file(file_name, string_to_search):
 
 location = 'lookingfor_a_line.py'
 line = input("what do you want to search in this program? Type it here: ", )
+
 try:
     print(search_string_in_file(location, line))
 except:
